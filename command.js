b75037391d8104c785f7ad827cfda3aacc1d5ab5
Elizabeth Sullivan <lyriafirelle@gmail.com>
2026-02-22 01:12:05 -0800

    Moved the piecharts into a separate file in preparation for merge
    Moved the piecharts into a separate file in preparation for merge.

--- command.js
+++ command.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 const commands = [
-  new SlashCommandBuilder().setName("ping").setDescription("Replies with pong!"),
-  new SlashCommandBuilder().setName("sprite").setDescription("Puts a sprite image in chat."),
-  new SlashCommandBuilder().setName("testpiechart").setDescription("Tests the pie chart."),
-  new SlashCommandBuilder().setName("teststatement").setDescription("Tests the statement."),
+  new SlashCommandBuilder().setName("statement").setDescription("Tests the statement."),
 ].map(c => c.toJSON());
 
 (async () => {
