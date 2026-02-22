1316161482553aff2ed7a6f4a8e25aaf33061abe
Elizabeth Sullivan <lyriafirelle@gmail.com>
2026-02-21 21:56:56 -0800

    Changed the pie chart legend display and embed format
    Changed the pie chart legend display and embed format.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits, AttachmentBuilder, EmbedBuilder, Message, Attachment } = require("discord.js");
 const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
-const { createCanvas, loadImage } = require('canvas');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const pieChart = new ChartJSNodeCanvas({width: 500, height: 500}); //pixel x pixel size
 const pieChart2 = new ChartJSNodeCanvas({width: 500, height: 500}); //pixel x pixel size
+const pieChart3 = new ChartJSNodeCanvas({width: 500, height: 500}); //pixel x pixel size
 const date = new Date();
 
 client.once("clientReady", () => {
@@ -49,40 +49,68 @@ client.on("interactionCreate", async (interaction) => {
     const pieConfig2 = {
       type: 'pie',
       data: {
-        labels: ['Apples', 'Bananas', 'Cherries', 'Dates'],
+        labels: ['Apples', 'Bananas', 'Cherries', 'Dates', 'Peaches', 'Pears', 'Blueberries', 'Strawberries', 'Kiwis', 'Oranges'],
         datasets: [{
-          data: [15, 30, 45, 10],
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#8AFF33'],
+          data: [5, 5, 15, 5, 5, 25, 15, 15, 5, 5],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#8AFF33', '#acc9ff', '#f6ffb2', '#d67979', '#9985f0', '#bcffce', '#06f5ed'],
         }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: 'right', // vertical legend
+            labels: {
+              boxWidth: 30,    // bigger color boxes
+              font: {size: 18}, //bigger text
+              padding: 20      // spacing between items
+            }
+          }
+        }
       }
     };
 
     const image2 = await pieChart2.renderToBuffer(pieConfig2);
+    const pie2 = new AttachmentBuilder(image2, { name: 'Image2.png' });
 
-    const pieImage2 = new AttachmentBuilder(image2, { name: 'piechart2.png' });
+      const pieConfig3 = {
+      type: 'pie',
+      data: {
+        labels: ['Apples', 'Bananas', 'Cherries', 'Dates', 'Peaches', 'Pears', 'Blueberries', 'Strawberries', 'Kiwis', 'Oranges'],
+        datasets: [{
+          data: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#8AFF33', '#acc9ff', '#f6ffb2', '#d67979', '#9985f0', '#bcffce', '#06f5ed'],
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: 'right', // vertical legend
+            labels: {
+              boxWidth: 30,    // bigger color boxes
+              font: {size: 18}, //bigger text
+              padding: 20      // spacing between items
+            }
+          }
+        }
+      }
+    };
 
-    //combine two images:
-    const img1 = await loadImage(image2);
-    const img2 = await loadImage(image2);
-    const canvas = createCanvas((img1.width + img2.width), (img1.height + img2.height));
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img1, 0, 0);
-    ctx.drawImage(img2, img1.width, 0);
-    const combinedImage = canvas.toBuffer();
-    const comboImage = new AttachmentBuilder(combinedImage, { name: 'comboImage.png' });
+    const image3 = await pieChart3.renderToBuffer(pieConfig3);
+    const pie3 = new AttachmentBuilder(image3, { name: 'Image3.png' });
 
     //The embed for our statement that holds the information.
     const statement = new EmbedBuilder()
-      .setTitle('Here is today\'s Statement:')
-      .setDescription('Today\'s Date: ' + (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear())
-      .setImage('attachment://comboImage.png')
+      .setColor('#feffb9')
+      .setTitle((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear() + ' Statement')
+      .setDescription('Below is today\'s breakdown:')
       .addFields(
-        {name: 'Planned Time Allotment', value: ' ', inline: true},
-        {name: 'Actual Time Allotment', value: ' ', inline: true},
+        {name: '(+/-)some field here: some number here', value: ' ', inline: false},
       )
-      .setTimestamp()
+      .setTimestamp();
 
-    interaction.reply({ embeds: [statement], files: [comboImage]});
+    interaction.reply({ embeds: [statement], files: [image2, image3]});
   }
 
 });
